d9793221c2922cfa5379dfc40ec62a656bff6ba2
Simon Cropp <simon.cropp@gmail.com>
2021-05-31 10:41:21 +1000

    fix RS1032
    Severity	Code	Description	Project	File	Line	Suppression State
    Error	RS1032	The diagnostic message should not contain any line return character nor any leading or trailing whitespaces and should either be a single sentence without a trailing period or a multi-sentences with a trailing period	EntityFrameworkCore.Projections.Generator	C:\Code\EntityFrameworkCore.Projections\src\EntityFrameworkCore.Projections.Generator\Diagnostics.cs	15	Active

--- src/EntityFrameworkCore.Projectables.Generator/Diagnostics.cs
+++ src/EntityFrameworkCore.Projectables.Generator/Diagnostics.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 
 namespace EntityFrameworkCore.Projectables.Generator
 {
@@ -12,10 +7,9 @@ namespace EntityFrameworkCore.Projectables.Generator
         public static readonly DiagnosticDescriptor RequiresExpressionBodyDefinition = new(
             id: "EFP0001",
             title: "Method or property should expose an expression body definition",
-            messageFormat: "Method or property '{0}' should expose an expression body definition.",
+            messageFormat: "Method or property '{0}' should expose an expression body definition",
             category: "Design",
             DiagnosticSeverity.Error,
             isEnabledByDefault: true);
-
     }
 }
