97bbc003fe7459fe908e132c180d38ab27604c0d
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-11-15 20:34:36 +0800

    Added minimal docs on the projectable attribute

--- src/EntityFrameworkCore.Projectables.Abstractions/ProjectableAttribute.cs
+++ src/EntityFrameworkCore.Projectables.Abstractions/ProjectableAttribute.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCore.Projectables
 {
+    /// <summary>
+    /// Mark this method or property as a Projectable
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public sealed class ProjectableAttribute : Attribute
     {
+        /// <summary>
+        /// Get or set how null-conditional operators are handeled
+        /// </summary>
         public NullConditionalRewriteSupport NullConditionalRewriteSupport { get; set; }
     }
 }
