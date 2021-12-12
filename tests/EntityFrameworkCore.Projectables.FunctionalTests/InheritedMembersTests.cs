e9405b5933627f68719256336d715724f18dd420
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-12-12 20:03:59 +0800

    Added a functional test

---
+++ tests/EntityFrameworkCore.Projectables.FunctionalTests/InheritedMembersTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Projectables.FunctionalTests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using VerifyXunit;
+using Xunit;
+
+namespace EntityFrameworkCore.Projectables.FunctionalTests
+{
+    [UsesVerify]
+    public class InheritedMembersTests
+    {
+        public abstract class Animal
+        {
+            public int Id { get; set; }
+            public double AverageLifespan { get; set; }
+            public int Age { get; set; }
+
+            [Projectable]
+            public double LifeProgression
+                => Age / AverageLifespan;
+        }
+
+        public class Cat : Animal
+        {
+            public int MentalAge { get; set; }
+
+            [Projectable]
+            public double MentalLifeProgression
+                => MentalAge / AverageLifespan;
+
+            [Projectable]
+            public double ReservedAge
+                => base.Age; // base should not affect the generated outcome
+        }
+
+
+        [Fact]
+        public Task ProjectOverMethodTakingDbContext()
+        {
+            using var dbContext = new SampleDbContext<Cat>();
+
+            var query = dbContext.Set<Cat>()
+                .Select(x => new { x.LifeProgression, x.AverageLifespan });
+
+            return Verifier.Verify(query.ToQueryString());
+        }
+    }
+}
