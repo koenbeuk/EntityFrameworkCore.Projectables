68afb8fa500e42fdad3ab4135da4157a0d44e51b
Koen <koen@linker.io>
2022-10-11 15:13:01 +0100

    Support default parameter values

---
+++ tests/EntityFrameworkCore.Projectables.FunctionalTests/DefaultValueTests.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameworkCore.Projectables.FunctionalTests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+namespace EntityFrameworkCore.Projectables.FunctionalTests
+{
+    [UsesVerify]
+    public class DefaultValueTests
+    {
+        public record Entity
+        {
+            public int Id { get; set; }
+
+            [Projectable]
+            public int NextId(int skip = 1) => Id + skip;
+        }
+
+        [Fact]
+        public Task ExplicitDefaultValueIsSupported()
+        {
+            using var dbContext = new SampleDbContext<Entity>();
+
+            var query = dbContext.Set<Entity>()
+                .Select(x => x.NextId(2));
+
+            return Verifier.Verify(query.ToQueryString());
+        }
+    }
+}
