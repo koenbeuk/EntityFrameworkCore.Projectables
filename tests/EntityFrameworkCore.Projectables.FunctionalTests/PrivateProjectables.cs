2295e29125218f36c3975017ef67a48d805295a7
Koen <koen@linker.io>
2023-03-16 00:23:06 +0000

    Don't throw on private and protected properties

---
+++ tests/EntityFrameworkCore.Projectables.FunctionalTests/PrivateProjectables.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Projectables.FunctionalTests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using VerifyXunit;
+using Xunit;
+
+namespace EntityFrameworkCore.Projectables.FunctionalTests
+{
+    [UsesVerify]
+    public class PrivateProjectables
+    {
+        public record Entity
+        {
+            public int Id { get; set; }
+        }
+
+        bool IsAdmin => true;
+
+        [Fact]
+        public Task Issue63Repro()
+        {
+            using var dbContext = new SampleDbContext<Entity>();
+
+            var query = dbContext.Set<Entity>()
+                    .Where(product => IsAdmin || product.Id == 1);
+
+            return Verifier.Verify(query.ToQueryString());
+        }
+    }
+}
