dadb5f33a0bb0b9d2b64f16944a37d25c45b03a1
fabien.menager <fabien.menager@am-creations.fr>
2024-03-05 21:51:33 +0100

    Fix QualifiedNameSyntax visiting (fixes #100)

---
+++ tests/EntityFrameworkCore.Projectables.FunctionalTests/NameSyntaxTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Projectables.FunctionalTests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using VerifyXunit;
+using Xunit;
+
+namespace EntityFrameworkCore.Projectables.FunctionalTests
+{
+    public class Local
+    {
+        [Flags]
+        public enum SampleEnum
+        {
+            One  = 0b001,
+            Two  = 0b010,
+            Four = 0b100,
+        }
+    }
+    
+    [UsesVerify]
+    public class NameSyntaxTests
+    {
+        public class Entity
+        {
+            public int Id { get; set; }
+            
+            [Projectable]
+            public Local.SampleEnum? Test => Local.SampleEnum.One | Local.SampleEnum.Two | Local.SampleEnum.Four;
+        }
+
+        [Fact]
+        public Task QualifiedNameSyntaxTest()
+        {
+            using var dbContext = new SampleDbContext<Entity>();
+
+            var query = dbContext.Set<Entity>()
+                .Select(x => x.Test);
+
+            return Verifier.Verify(query.ToQueryString());
+        }
+    }
+}
